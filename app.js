714a29f2af1cfa3dcc7386847d56db9995125c47
LAPTOP-HOKG2C4K\ASUS <longtran4949@gmail.com>
2023-04-02 22:10:29 +0700

    db connect 3

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://larrytran49:larrytran49@larrytran.gdyab.mongodb.net/advance_tool?retryWrites=true&w=majority'
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect(MONGO_URL, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("DB is ready"))
+  .catch((err) => console.log(err));
 
 app.use('/notes', noteRoutes);
 
