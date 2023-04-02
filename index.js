2b90feb6136c0aaec55f6f2738e72fd98046cc99
LAPTOP-HOKG2C4K\ASUS <longtran4949@gmail.com>
2023-04-02 22:44:54 +0700

    change file name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const noteRoutes = require('../advance_tool/routes/note.route');
+const noteRoutes = require('./routes/note.route');
 require("dotenv").config({ path: "./config.env" });
 
 const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://larrytran49:larrytran49@larrytran.gdyab.mongodb.net/advance_tool?retryWrites=true&w=majority'
