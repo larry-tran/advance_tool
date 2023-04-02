3f4535fd2d8271ec69a50d3d7d332efa8f4c39e9
LAPTOP-HOKG2C4K\ASUS <longtran4949@gmail.com>
2023-04-02 21:34:36 +0700

    second commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config({ path: "./config.env" });
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://larrytran49:larrytran49@larrytran.gdyab.mongodb.net/advance_tool?retryWrites=true&w=majority'
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
