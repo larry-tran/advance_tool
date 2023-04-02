14a33cf8af6c8bb8968f72b889ad1305f181f76d
LAPTOP-HOKG2C4K\ASUS <longtran4949@gmail.com>
2023-04-02 22:35:57 +0700

    connect db 5

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const noteRoutes = require('../server/routes/note.route');
+const noteRoutes = require('../advance_tool/routes/note.route');
 require("dotenv").config({ path: "./config.env" });
 
-const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://larrytran49:larrytran49@larrytran.gdyab.mongodb.net/advance_tool?retryWrites=true&w=majority'
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,7 +16,7 @@ mongoose
 
 app.use('/notes', noteRoutes);
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on Port: 3000");
 });
 
